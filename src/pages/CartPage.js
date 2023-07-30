e0173a8438b431e9dd3d724548a4b01630c4fd1a
ramkishan <ramkishan00128@gmail.com>
2023-07-31 00:00:47 +0530

    second commit

---
+++ src/pages/CartPage.js
@@ -0,0 +1,7 @@
+import Cart from "../features/cart/Cart";
+
+const CartPage = () => {
+  return <Cart></Cart>;
+};
+
+export default CartPage;
---
+++ src/pages/SignupPage.js
@@ -0,0 +1,6 @@
+import { Signup } from "../features/auth/components/Signup";
+const SignupPage = () => {
+  return <Signup />;
+};
+
+export default SignupPage;
